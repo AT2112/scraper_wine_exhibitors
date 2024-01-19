673ab6a413071e0c813e49b90daa242239f7a999
AT2112 <andry.timoshenko@gmail.com>
2024-01-19 19:19:26 +0100

    Initial commit

--- scraper.py
+++ scraper.py
@@ -2,8 +2,7 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait, Select
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
 import csv
